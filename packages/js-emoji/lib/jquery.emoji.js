899a47cf86b1ad9c23737e9d9d80af55ad0ec6bf
John Hannagan <weetbix_@hotmail.com>
2015-12-27 00:26:39 +0800

    tarball node packages (facebook-chat-api with mods, js-emoji)

--- packages/js-emoji/lib/jquery.emoji.js
+++
@@ -1,8 +0,0 @@
-// A very simple jQuery wrapper for js-emoji
-$.fn.emoji = function(){
-	return this.each(function(){
-		$(this).html(function (i, oldHtml){
-			return emoji.replace_colons(oldHtml);
-		});
-	});
-};
